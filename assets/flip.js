bc77e396a1e234ab1b54d5f109c792e85f3c15e4
Maza455 <same7nte@gmail.com>
2024-05-31 15:40:53 +0200

    Adding badges

---
+++ assets/flip.js
@@ -0,0 +1,6 @@
+// function flipCard(card) {
+//     const front = card.querySelector('.flip-card-front');
+//     const back = card.querySelector('.flip-card-back');
+//     front.classList.toggle('hidden');
+//     back.classList.toggle('hidden');
+//   }
